cdac01a4ec82286468842466688b0bf0d14a08b6
abn-jp <90902291+abn-jp@users.noreply.github.com>
2026-05-11 00:09:29 +0900

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -11,5 +11,6 @@ get_header();
 <?php get_template_part( 'template-parts/featured-products' ); ?>
 <?php get_template_part( 'template-parts/halal-info' ); ?>
 <?php get_template_part( 'template-parts/testimonials' ); ?>
+<?php get_template_part( 'template-parts/latest-posts' ); ?>
 
 <?php get_footer(); ?>
